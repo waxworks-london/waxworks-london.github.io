de21ca8ab8edd9ff9f846b8b1bb2c747f304b57a
Tim Kendrick <timkendrick@gmail.com>
2025-01-18 14:51:30 +0000

    [WIP] green intro colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+const body = {
+  primary: 'hsl(235, 66.67%, 30%)',
+  secondary: 'hsl(235, 30%, 30%)',
+};
+
+const accent = {
+  background: 'hsl(181, 45%, 81%)',
+  primary: body.primary,
+  secondary: colors.cyan['700'],
+};
+
+const button = {
+  primary: colors.red['500'],
+};
 
 module.exports = {
   content: ['./src/**/*.{html,js}'],
@@ -9,6 +25,26 @@ module.exports = {
         sans: ['Libre Franklin', ...defaultTheme.fontFamily.sans],
         serif: ['Prata', ...defaultTheme.fontFamily.serif],
       },
+    },
+    colors: {
+      ...colors,
+      white:  'hsl(42 42% 96%);',
+      body,
+      accent,
+      button,
+      // gray: {
+      //   ...colors.gray,
+      //   '600': 'hsl(235, 30%, 30%)',
+      //   '900': 'hsl(235, 66.67%, 30%)',
+      // },
+      // indigo: {
+      //   ...colors.indigo,
+      //   '100': 'hsl(181, 45%, 81%)'
+      // },
+      // intro: {
+      //   'primary': body.primary,
+      //   'secondary': colors.cyan['700'],
+      // },
     },
   },
   plugins: [],
